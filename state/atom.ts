d32e06cbc9035a8cef05094207535433bb3ee51f
bongsu <11t518s20161759@gmail.com>
2022-09-23 00:46:35 +0900

    deploy 4

--- state/atom.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from "recoil";
-
-export const nicknameAtom = atom({
-  key: "nickname",
-  default: "기본닉네임",
-});
--- state/refresh.trigger.atom.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from "recoil";
-
-export const _dustInfoRefresher = atom<number>({
-  key: "_dustInfo",
-  default: 0,
-});
